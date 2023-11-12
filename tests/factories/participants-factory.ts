8e412ed47ac30c4dac6e1a74f1dc0760da8b9280
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-11-11 23:04:35 -0300

    test: add new tests for /participants

---
+++ tests/factories/participants-factory.ts
@@ -0,0 +1,33 @@
+import prisma from "../../src/database/database";
+import { faker } from '@faker-js/faker';
+import { createParticipantType } from "../../src/schemas/participantsSchema";
+
+export function createParticipantInfo(): createParticipantType {
+    return {
+        name: faker.internet.userName(),
+        balance: faker.number.int({ min: 1000, max: 500000 })
+    }
+}
+export async function createManyParticipants(numberOfParticipants: number): Promise<void> {
+    const participantsData = [];
+    for (let i = 0; i < numberOfParticipants; i++) {
+        participantsData.push({
+            name: faker.internet.userName(),
+            balance: faker.number.int({ min: 1000, max: 500000 })
+        });
+    };
+    await prisma.participant.createMany({
+        data: participantsData,
+    });
+    return;
+}
+
+
+/*
+return await prisma.participant.create({
+        data:{
+            name: faker.internet.userName(),
+            balance: faker.number.int({ min: 1000 })
+        }
+    })
+    */
--- tests/participants.test.ts
+++ tests/participants.test.ts
@@ -3,6 +3,7 @@ import app from "../src/app";
 import prisma from "../src/database/database";
 import { cleanDb } from "./services/helpers";
 import httpStatus from "http-status";
+import { createManyParticipants, createParticipantInfo } from "./factories/participants-factory";
 
 const api = supertest(app);
 
@@ -16,15 +17,55 @@ describe("POST /participants", () => {
         expect(response.status).toBe(httpStatus.BAD_REQUEST);
     });
     it("Should respond with status 422 when body is incomplete", async () => {
-        const response = await api.post('/participants').send();
-        expect(response.status).toBe(httpStatus.BAD_REQUEST);
+        const response = await api.post('/participants').send({name: "Lucas"});
+        expect(response.status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
     });
     describe('When body is valid', () => {
-        it("Should respond with status 400 when the balance is less than R$10,00", async () => {
-
+        it("Should respond with status 400 when the balance is less than R$10,00 (1000)", async () => {
+            const response = await api.post('/participants').send({name: "Lucas", balance: 10});
+            expect(response.status).toBe(httpStatus.BAD_REQUEST);
         });
         it("Should respond with status 201 when the participant is created", async () => {
-
+            const newParticipant = createParticipantInfo();
+            const response = await api.post('/participants').send({name: newParticipant.name, balance: newParticipant.balance});
+            expect(response.status).toBe(httpStatus.CREATED);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    id: expect.any(Number),
+                    createdAt: expect.any(String),
+                    updatedAt: expect.any(String),
+                    name: newParticipant.name,
+                    balance: newParticipant.balance
+                })
+            )
         });
+    })
+})
+
+describe("GET /participants", () => {
+    it("should return an empty array if there are no registered participants.",async () => {
+        const response = await api.get('/participants');
+        console.log("AQUIIIIIIIIIIIIIIIIII", response.body);
+        console.log(response.body);
+        expect(response.status).toBe(httpStatus.OK);
+        expect(response.body).toStrictEqual([]);
+    });
+    it("Should return all participants and status 200.",async () => {
+        const numberOfParticipants = 3;
+        createManyParticipants(numberOfParticipants);
+        const response = await api.get('/participants');
+        expect(response.status).toBe(httpStatus.OK);
+        expect(response.body).toHaveLength(numberOfParticipants);
+        expect(response.body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    id: expect.any(Number),
+                    createdAt: expect.any(String),
+                    updatedAt: expect.any(String),
+                    name: expect.any(String),
+                    balance: expect.any(Number)
+                })
+            ])
+        )
     })
 })
